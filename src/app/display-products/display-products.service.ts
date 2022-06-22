291a7623dcc8c5c520505c707c63342936bc9679
pranav3850 <pranavgoswami38@gmail.com>
2022-06-22 17:01:20 +0530

    new merged commit

--- src/app/display-products/display-products.service.ts
+++ src/app/display-products/display-products.service.ts
@@ -2,16 +2,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ApiService } from 'app/api.service';
-<<<<<<< HEAD
 import { Category } from 'app/products/category.model';
 import { Products } from 'app/products/product.model';
 import { Observable } from 'rxjs';
 
-=======
-import { Observable } from 'rxjs';
-import { Category } from 'app/Products/category.model';
-import { Products } from 'app/Products/product.model';
->>>>>>> 166ff5debb2f2cf6325b2a18b5784c26943b3e8b
 
 @Injectable({
     providedIn: 'root'
